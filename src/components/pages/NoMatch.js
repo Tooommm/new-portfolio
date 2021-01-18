942848d9968bbfac122342efe50d523bd424bbb3
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-01-18 11:34:31 +0100

    heroku

--- src/components/pages/NoMatch.js
+++ src/components/pages/NoMatch.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './NoMatch.css';
 
 const NoMatch = () => {
   return (
-    <div>
-      <h1>You're lost! </h1>
-      <p>Nothing to do with the TV show</p>
-      <p>
-        <code>{window.location.href}</code> <br/>
-        N'existe pas sur thomasbrouillet.com ou pas encore...
-      </p>
+    <div className="no-match">
+      <div className="container">
+        <div className="title">
+          <h1>You're lost! </h1>
+        </div>
+        <div className="content">
+          <br/>
+          <p>Nothing to do with the TV show...</p>
+          <br/>
+          <p>
+            <code>{window.location.href}</code> <br/>
+            Does not exist on thomasbrouillet.com or not yet!
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
