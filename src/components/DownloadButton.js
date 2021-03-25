080439aeec0b04db03652227d300f756bd596678
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-03-25 12:26:49 +0100

    Thomas Brouillet French Developer

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -5,14 +5,14 @@ import cv from "../assets/CV_Thomas_Brouillet.pdf";
 
 const DownloadButton = () => {
   return (
-    <a class="face-button" href={cv} download>
-      <div class="face-primary">
-        <span class="icon fa fa-cloud"></span>
+    <a className="face-button" href={cv} download>
+      <div className="face-primary">
+        <span className="icon fa fa-cloud"></span>
         Mon CV
       </div>
 
-      <div class="face-secondary">
-        <span class="icon fa fa-hdd-o"></span>
+      <div className="face-secondary">
+        <span className="icon fa fa-hdd-o"></span>
         Size: 152 ko
       </div>
     </a>
